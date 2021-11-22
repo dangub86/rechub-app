9a5475f715e3d5f708c5d12c71982db3c5399293
Daniele Gubbiotti <gubbiottidaniele@gmail.com>
2021-11-22 14:10:50 +0100

    Datastax Astra Cassandra configuration

---
+++ rechub/src/main/java/app/pickmaven/rechub/config/DatastaxAstraConfig.java
@@ -0,0 +1,20 @@
+package app.pickmaven.rechub.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+@Configuration
+public class DatastaxAstraConfig {
+    @Value("${datastax.astra.secure-connect-bundle}")
+    private File cloudSecureBundle;
+
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer() {
+        return builder ->
+                builder.withCloudSecureConnectBundle(cloudSecureBundle.toPath());
+    }
+}
